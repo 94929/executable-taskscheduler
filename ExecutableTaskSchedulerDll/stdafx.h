ff4279b048ac069c80b7ca73801010dcb1ccc347
jsung5381 <jsung5381@naver.com>
2016-12-16 09:35:35 +0900

    Imported first skeleton codes from MSDN.

--- ExecutableTaskSchedulerDll/stdafx.h
+++ ExecutableTaskSchedulerDll/stdafx.h
@@ -10,6 +10,15 @@
 #include <stdio.h>
 #include <tchar.h>
 
-
-
 // TODO: reference additional headers your program requires here
+#define _WIN32_DCOM
+
+#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <comdef.h>
+
+//  Include the task header file.
+#include <taskschd.h>
+#pragma comment(lib, "taskschd.lib")
+#pragma comment(lib, "comsupp.lib")
